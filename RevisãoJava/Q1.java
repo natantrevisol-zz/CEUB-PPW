c95e431cc4bbc406655837824a75c738d15e539f
Natan Trevisol <42854525+natantrevisol@users.noreply.github.com>
2021-04-07 17:08:10 -0300

    Create Q1.java

---
+++ RevisãoJava/Q1.java
@@ -0,0 +1,9 @@
+package revisaojavapwnatan;
+
+public class RevisaoJavaPWNatan {
+    public static void main(String[] args) {
+        for(int i=150; i<=300; i++){
+            System.out.println(i);
+        }
+    }
+}
