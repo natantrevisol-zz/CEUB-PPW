99b8f083ae1bb8f56c641e9bcfe426c215145f25
Natan Trevisol <42854525+natantrevisol@users.noreply.github.com>
2021-04-07 17:08:39 -0300

    Update Q1.java

--- RevisãoJava/Q1.java
+++ RevisãoJava/Q1.java
@@ -1,5 +1,9 @@
 package revisaojavapwnatan;
 
+/*
+Desenvolva um programa que imprima todos os números entre 150 e 300, um em cada linha.
+*/
+
 public class RevisaoJavaPWNatan {
     public static void main(String[] args) {
         for(int i=150; i<=300; i++){
